44583953366f3047eb6ba2b62cbdebcc07b5bbba
Istabrak Abbes <74003999+istabrak-abbes@users.noreply.github.com>
2025-01-10 23:51:05 -0500

    Update tkhoubith.sh

--- tkhoubith.sh
+++ tkhoubith.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-"Starting script with PID $$" >> nohup.out
-
-i=5
-
-while [ $i -lt 10 ]; do
-	echo "Iteration $i:"
-	salloc -A bif151 -N 32  -t 2:00:00 -q debug <<EOF >> 510M_joint_${i}.log 2>&1
-	source joint_training_script.sh
-EOF
-	i=$((i + 1))
-        sleep 20000
-done
 
